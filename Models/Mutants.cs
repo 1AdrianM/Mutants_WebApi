b9274409aaddaedd7ee50a2aa1ffbb9ab2909096
1AdrianM <20198252@itla.edu.do>
2024-05-13 23:37:54 -0400

    Adding Models part 1

---
+++ Models/Mutants.cs
@@ -0,0 +1,18 @@
+namespace Xmen_WebApi.Model;
+
+public class Mutants
+{
+    public int MutantId {get; set;}
+     public string Name {get; set;}
+    public string Alias {get; set;}
+    public string LivingStatus {get; set;}
+    public string Height {get; set;}
+    public string Weight {get; set;}
+    public string Img_Path {get; set;}
+    public string Aligntment {get; set;}
+    public string Mutation{get; set;}
+    public string Abilities {get; set;}
+    public string Classification{get; set;}
+    public int PowerLevel {get; set;}
+
+}
